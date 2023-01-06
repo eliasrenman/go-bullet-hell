ca838b22038149d36399989f9eb7708efe8a314d
Elias Renman <elias.renman00@gmail.com>
2023-01-06 17:39:48 +0100

    feat: added displaying of healthbars

--- gui.go
+++ gui.go
@@ -10,25 +10,45 @@ import (
 	"golang.org/x/image/font/opentype"
 )
 
+type GuiElement interface {
+	Draw(screen *ebiten.Image)
+	Update()
+	Start()
+}
+
+var GuiElements = make(map[GuiElement]struct{})
+
 type GuiText struct {
 	font font.Face
 	*Vector
-	text string
-}
-
-func NewGuiText(font font.Face, x float64, y float64) *GuiText {
-	return &GuiText{
-		font: LoadFont("fonts/FiraCode.ttf", opentype.FaceOptions{}),
-	}
+	text         string
+	displayLabel string
 }
 
 func (menu *GuiText) Draw(screen *ebiten.Image) {
-
-	text.Draw(screen, "debugText", menu.font, int(menu.X), int(menu.Y), color.White)
+	text.Draw(screen, menu.text, menu.font, int(menu.X), int(menu.Y), color.White)
 
 }
 
 func (menu *GuiText) Update() {
+}
+
+func (menu *GuiText) Start() {
+}
+
+func NewGuiHealthBar(health *Health, X float64, Y float64, label string) *GuiHealthBar {
+	return &GuiHealthBar{
+		GuiText: &GuiText{
+			font: LoadFont("fonts/FiraCode.ttf", opentype.FaceOptions{}),
+			text: "Health: 100%",
+			Vector: &Vector{
+				X: X,
+				Y: Y,
+			},
+			displayLabel: label,
+		},
+
+		Health: health}
 }
 
 type GuiHealthBar struct {
@@ -38,5 +58,10 @@ type GuiHealthBar struct {
 
 func (healthBar *GuiHealthBar) Update() {
 	precentage := int((float64(healthBar.Health.HitPoints) / float64(healthBar.Health.MaxHitPoints)) * 100)
-	healthBar.text = fmt.Sprintf("Health: %d %%, %d/%d", precentage, healthBar.Health.HitPoints, healthBar.Health.MaxHitPoints)
+	healthBar.text = fmt.Sprintf("%s Health: %d %%, %d/%d", healthBar.displayLabel, precentage, healthBar.Health.HitPoints, healthBar.Health.MaxHitPoints)
+}
+
+func SpawnHealthBar[T GuiElement](obj T) T {
+	GuiElements[obj] = struct{}{}
+	return obj
 }
